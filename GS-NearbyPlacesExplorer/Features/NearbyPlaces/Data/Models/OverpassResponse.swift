661e72b237bdfcc1f66aa9b32e8077e0a2d08b90
Carlos Guerra <guerrier@outlook.com>
2026-08-16 20:08:11 -0600

    [AI FEAT]: Add OverpassResponse DTO

---
+++ GS-NearbyPlacesExplorer/Features/NearbyPlaces/Data/Models/OverpassResponse.swift
@@ -0,0 +1,32 @@
+//
+//  OverpassResponse.swift
+//  GS-NearbyPlacesExplorer
+//
+//  Created by Carlos Lopez on 13/08/26.
+//
+
+import Foundation
+
+public struct OverpassResponse: Decodable, Sendable {
+    public let elements: [OSMElement]
+}
+
+public struct OSMElement: Decodable, Sendable {
+    public let type: String
+    public let id: Int
+    public let lat: Double
+    public let lon: Double
+    public let tags: OSMTags?
+}
+
+public struct OSMTags: Decodable, Sendable {
+    public let amenity: String?
+    public let name: String?
+    public let openingHours: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case amenity
+        case name
+        case openingHours = "opening_hours"
+    }
+}
