469243bb285a47ea100e4c20efaeed33b14f6bad
Carlos Guerra <guerrier@outlook.com>
2026-08-17 01:06:22 -0600

    [AI FEAT]: Add protocols for favorite places data source

---
+++ GS-NearbyPlacesExplorer/Features/AboutThePlace/Data/DataSource/Protocols/FavoritePlacesDataSourcesProtocols.swift
@@ -0,0 +1,13 @@
+//
+//  FavoritePlacesDataSourcesProtocols.swift
+//  GS-NearbyPlacesExplorer
+//
+//  Created by Carlos Guerra
+//
+
+import Foundation
+
+public protocol FavoritePlacesLocalDataSource: Sendable {
+    func isFavorite(osmId: Int, userEmail: String) async throws -> Bool
+    func toggleFavorite(entity: FavoritePlaceEntity) async throws
+}
